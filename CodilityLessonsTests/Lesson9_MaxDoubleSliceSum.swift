c517af6c9487450cde9b342955079aa01e9f98a4
Oleksandr Malovichko <a@madappgang.com>
2017-04-16 21:16:55 +0300

    Update algorithm

--- CodilityLessonsTests/Lesson9_MaxDoubleSliceSum.swift
+++ CodilityLessonsTests/Lesson9_MaxDoubleSliceSum.swift
@@ -11,20 +11,40 @@ import XCTest
 class Lesson9_MaxDoubleSliceSum: XCTestCase {
     
     func test() {
-        var arr0 = [3, 2, 6, -20, 4, 5, -1, 2]
-        XCTAssertEqual(solution(&arr0), 8 + 9)
+        var a = [Int]()
         
-        var arr = [3, 2, 6, -1, 4, 5, -1, 2]
-        XCTAssertEqual(solution(&arr), 8 + 9)
+        a = [0, 1, -1, -1, 10, 0]
+        XCTAssertEqual(solution(&a), 9)
         
-        var arr2 = [3, 2, 6, -1, 4, 5, -1, -1, -1, -1, -1, -1, 2]
-        XCTAssertEqual(solution(&arr2), 8 + 9)
+        a = [3, 2, 6, -20, 4, 5, -1, 2]
+        XCTAssertEqual(solution(&a), 8 + 9)
         
-        var arr3 = [1, 2, -1, 2, 1]
-        XCTAssertEqual(solution(&arr3), 4)
+        a = [3, 2, 6, -1, 4, 5, -1, 2]
+        XCTAssertEqual(solution(&a), 8 + 9)
         
-        var arr4 = [0, 2, -20, -20, 2, 0]
-        XCTAssertEqual(solution(&arr4), 2)
+        a = [3, 2, 6, -1, 4, 5, -1, -1, -1, -1, -1, -1, 2]
+        XCTAssertEqual(solution(&a), 8 + 9)
+        
+        a = [1, 2, -1, 2, 1]
+        XCTAssertEqual(solution(&a), 4)
+        
+        a = [0, 2, -20, -20, 2, 0]
+        XCTAssertEqual(solution(&a), 2)
+        
+        a = [6, 1, 5, 6, 4, 2, 9, 4]
+        XCTAssertEqual(solution(&a), 26)
+        
+        a = [0, -100, 1, 0]
+        XCTAssertEqual(solution(&a), 1)
+        
+        a = [0, 1, -100, 0]
+        XCTAssertEqual(solution(&a), 1)
+        
+        a = [0, 0, 1, 0]
+        XCTAssertEqual(solution(&a), 1)
+        
+        a = [0, -100, -100, -100, 1, -100, -100, -100, 0]
+        XCTAssertEqual(solution(&a), 1)
     }
     
     public func solution(_ A : inout [Int]) -> Int {
@@ -37,69 +57,61 @@ class Lesson9_MaxDoubleSliceSum: XCTestCase {
         
         let count = A.count
         
-        var maxDoubleSliceSum = 0
-        
-        var previousSlices = Dictionary<Int, Slice>()
-        
         var currentSlice = Slice(value: A.first!, startIndex: 0, endIndex: 0)
+
+        var slices = [0: Slice(value: A.first!, startIndex: 0, endIndex: 0)]
 
         for i in 1..<count {
             let value = A[i]
             
             if value > currentSlice.sum + value {
-                let sum = self.maxDoubleSliceSum(leftSlices: previousSlices, rightSlice: currentSlice)
-                maxDoubleSliceSum = max(maxDoubleSliceSum, sum)
-                
-                previousSlices[currentSlice.endIndex] = currentSlice
                 currentSlice = Slice(value: value, startIndex: i, endIndex: i)
-                
             } else {
                 currentSlice.addValue(value: value)
             }
+            
+            let slice = slices[currentSlice.startIndex]
+            if slice == nil {
+                slices[currentSlice.startIndex] = currentSlice
+            } else if let slice = slice, slice.sum < currentSlice.sum {
+                slices[currentSlice.startIndex] = currentSlice
+            }
         }
         
-        let sum = self.maxDoubleSliceSum(leftSlices: previousSlices, rightSlice: currentSlice)
-        maxDoubleSliceSum = max(maxDoubleSliceSum, sum)
+        var maxDoubleSliceSum = A.first!
+        
+        for (_, slice) in slices {
+            if slice.size() != count {
+                maxDoubleSliceSum = max(maxDoubleSliceSum, slice.sum)
+            }
+            maxDoubleSliceSum = max(maxDoubleSliceSum, slice.sum - slice.minValue)
+            if let rightSlice = slices[slice.endIndex + 2] {
+                maxDoubleSliceSum = max(maxDoubleSliceSum, slice.sum + rightSlice.sum)
+            }
+        }
         
         return maxDoubleSliceSum
     }
-
-    func maxDoubleSliceSum(leftSlices: Dictionary<Int, Slice>, rightSlice: Slice) -> Int {
-        var maxSum = 0
-        let rightSliceMaxSum = max(rightSlice.sum, rightSlice.sum - rightSlice.lastValue)
-        maxSum = max(rightSliceMaxSum, maxSum)
-        
-        let rightSliceMax = max(rightSlice.sum, rightSlice.sum - rightSlice.minValue)
-        maxSum = max(rightSliceMax, maxSum)
-        
-        if let leftSlice = leftSlices[rightSlice.startIndex - 2] {
-            maxSum = max(maxSum, leftSlice.sum + rightSliceMaxSum)
-        }
-        
-        if let leftSlice = leftSlices[rightSlice.startIndex - 1] {
-            maxSum = max(maxSum, leftSlice.sum - leftSlice.lastValue + rightSliceMaxSum)
-        }
-        return maxSum
-    }
     
-    class Slice {
+    struct Slice {
         var sum = 0
         var startIndex = 0
         var endIndex = 0
-        var lastValue = 0
         var minValue = 0
         
         init(value: Int, startIndex: Int, endIndex: Int) {
             self.startIndex = startIndex
             self.endIndex = endIndex
             sum = value
-            lastValue = value
             minValue = value
         }
         
-        func addValue(value: Int) {
+        func size() -> Int {
+            return endIndex - startIndex + 1
+        }
+        
+        mutating func addValue(value: Int) {
             sum += value
-            lastValue = value
             minValue = min(minValue, value)
             endIndex += 1
         }
